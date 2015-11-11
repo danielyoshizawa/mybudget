bf908ac9340770ad3080676e22dd2d2ee78d5921
danielyoshizawa <yoshidanielcwb@gmail.com>
2015-11-10 20:05:45 -0600

    Some refactoring

--- app/src/main/java/com/depaul/daniel/mybudget/EntryAdapter.java
+++ app/src/main/java/com/depaul/daniel/mybudget/EntryAdapter.java
@@ -1,14 +1,11 @@
 package com.depaul.daniel.mybudget;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class EntryAdapter extends BaseAdapter {
--- app/src/main/java/com/depaul/daniel/mybudget/MyBudget.java
+++ app/src/main/java/com/depaul/daniel/mybudget/MyBudget.java
@@ -1,16 +1,9 @@
 package com.depaul.daniel.mybudget;
 
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -24,13 +17,18 @@ public class MyBudget extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_budget);
 
+        inflate();
+        configure();
+    }
+
+    private void inflate() {
         addButton = (Button) findViewById(R.id.button_add_activity);
-
         listView = (ListView) findViewById(R.id.entry_list);
+    }
+
+    private void configure() {
         listView.setAdapter(new EntryAdapter(this));
-
         listView.setOnItemClickListener(new ListViewOnClickListener(this, MyBudget.this));
-
         addButton.setOnClickListener(new AddOnClickListener(this, MyBudget.this));
     }
 
